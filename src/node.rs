a5caf8a09184477258f373511d4099ea55955d35
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2015-07-15 18:43:06 +0530

    feat(node::Data)

--- src/node.rs
+++ src/node.rs
@@ -1,1 +1,9 @@
+use std::collections::HashMap;
+
 pub type Ref = usize;
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum Data {
+    Text(String),
+    Element(String, HashMap<String, String>, Vec<Ref>)
+}
