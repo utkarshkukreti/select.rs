5fca4f8a13ad8c277a735255832178f98bb4abb0
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2015-07-15 20:42:00 +0530

    feat(predicate::Id)

--- src/predicate.rs
+++ src/predicate.rs
@@ -11,3 +11,11 @@ impl<T: AsRef<str>> Predicate for Name<T> {
         node.name() == Some(self.0.as_ref())
     }
 }
+
+pub struct Id<T>(pub T);
+
+impl<T: AsRef<str>> Predicate for Id<T> {
+    fn matches(&self, node: &Node) -> bool {
+        node.attr("id") == Some(self.0.as_ref())
+    }
+}
--- tests/predicate_tests.rs
+++ tests/predicate_tests.rs
@@ -9,10 +9,13 @@ pub use select::predicate::*;
 speculate! {
     describe "predicate" {
         before {
-            let dom = Dom::from_str("<html><head></head><body></body></html>");
+            let dom = Dom::from_str("<html><head></head><body>\
+<article id='post-0'></article>\
+</body></html>");
             let html = node::Node { dom: &dom, id: 0 };
             let head = node::Node { dom: &dom, id: 1 };
             let body = node::Node { dom: &dom, id: 2 };
+            let article = node::Node { dom: &dom, id: 3 };
         }
 
         test "Name()" {
@@ -25,6 +28,11 @@ speculate! {
             assert_eq!(Name("html").matches(&body), false);
             assert_eq!(Name("head").matches(&body), false);
             assert_eq!(Name("body").matches(&body), true);
+        }
+
+        test "Id()" {
+            assert_eq!(Id("post-0").matches(&html), false);
+            assert_eq!(Id("post-0").matches(&article), true);
         }
     }
 }
