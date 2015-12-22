b5e8014d2d2cc9b86f3c1212e21895ff2a8505e2
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2015-12-22 19:25:30 +0530

    feat: add some Node::attr benches

--- benches/benches.rs
+++ benches/benches.rs
@@ -53,5 +53,29 @@ speculate! {
                 b.iter(|| document.find(Comment));
             }
         }
+
+        context "Node::attr()" {
+            before {
+                let html = "<div a=b c=d e=f g=h i=j k=l m=n o=p q=r s=t u=v w=x y=z>";
+                let document = Document::from_str(html);
+                let node = document.nth(3);
+                assert_eq!(node.name(), Some("div"));
+            }
+
+            bench "hit first" |b| {
+                assert!(node.attr("a").is_some());
+                b.iter(|| node.attr("a"));
+            }
+
+            bench "hit last" |b| {
+                assert!(node.attr("y").is_some());
+                b.iter(|| node.attr("y"));
+            }
+
+            bench "miss" |b| {
+                assert!(node.attr("z").is_none());
+                b.iter(|| node.attr("z"));
+            }
+        }
     }
 }
